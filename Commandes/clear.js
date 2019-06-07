fcba29f61f4c27d1c93a7ae303a0bcf213405422
zephyytb <47774056+zephyytb@users.noreply.github.com>
2019-06-07 08:33:31 +0200

    Create clear.js

---
+++ Commandes/clear.js
@@ -0,0 +1,20 @@
+const discord = require("discord.js");
+
+module.exports.run = async(client, message, args) => {
+
+    if(!message.guild.member(message.author).hasPermission("MANAGE_MESSAGES")) return message.channel.send(`Vous n'avez pas la permission`).catch(console.error);
+
+    if(!message.guild.member(client.user).hasPermission("MANAGE_MESSAGES")) return message.channel.send(`Je n'ai pas la permission d'effectuer cette tâche`).catch(console.error);
+
+    if(!args[0]) return message.channel.send(`Vous devez spécifier un nombre de messages à supprimer`);
+
+    if(isNaN(args[0])) return message.channel.send(`Ce n'est pas un nombre`);
+
+    message.channel.bulkDelete(args[0]);
+
+    message.channel.send(`${args[0]} messages ont étés supprimés !`);
+};
+
+module.exports.help = {
+    name: "clear"
+}
